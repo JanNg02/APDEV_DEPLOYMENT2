70896bdaa614abf7a2742c76418d6ebcad20ca86
JDNG2002 <75112857+JDNG2002@users.noreply.github.com>
2022-11-25 11:10:45 +0800

    stuff

--- model/cartSchema.js
+++ model/cartSchema.js
@@ -4,8 +4,7 @@ const cartSchema = new mongoose.Schema( {
     username: { type: String},
     productName: { type: String},
     items: { type: Number },
-    price: { type: Number},
-        
+    price: { type: Number},     
 });
 //name of the schema, usersSchema (the var)
 module.exports = mongoose.model('cart', cartSchema);
